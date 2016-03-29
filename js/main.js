23a3cd6d7f5a34653d59489538571793e4e214c1
Kyle-Jackson <kylejackson@Kyles-MacBook-Pro.local>
2016-03-29 17:14:36 -0400

    Finished up to bounce in effect.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ $('.btn-show-hide').on('click', function (e) {
 $('.btn-move').on('click', function (e) {
   $('.diamond').toggleClass('slide');
 });
+
+$('.btn-collapse-expand').on('click', function (e) {
+  $('.panel').toggleClass('collapse');
+});
+
+$('.btn-bounce').on('click', function (e) {
+  $('.circle').toggleClass('bounce');
+});
